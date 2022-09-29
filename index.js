c34b59a99d873aa79c87cbffb1e29e3550122f60
Equinox <cameron.minchin@gmail.com>
2022-09-29 20:20:30 +0800

    no database

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
+
+app.get("/api/notes", (req, res) => {
+    let notes = {};
+    //read db
+
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    try {
+        let note = JSON.parse(req.body);
+    } catch (error) {
+        res.status(400).send("Cannot convert to JSON: " + error);
+        return;
+    }
+
+    // add to database
+
+    res.json(note);
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
